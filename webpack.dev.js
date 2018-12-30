6a12155a5c7f2b777538874d6550685be5d7f426
Alfonso Alba García <cursos@cursodegit.com>
2018-12-30 15:51:23 +0100

    Add webpack configuration to copy the canvas files from the src to the dist directory

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// https://github.com/gajus/write-file-webpack-plugin
+const path = require('path');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = {
+  // https://github.com/gajus/write-file-webpack-plugin
+  output: {
+    path: path.join(__dirname, './dist')
+  },
 
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
@@ -20,6 +29,13 @@ module.exports = {
 
   // https://webpack.js.org/concepts/plugins/
   plugins: [
+    new WriteFilePlugin(),
+    new CopyWebpackPlugin([
+      {from: './src/css', to: 'css'},
+      {from: './src/style.css', to: 'style.css'},
+      {from: './src/images', to: 'images'},
+      {from: './src/js', to: 'js'},
+    ]),
     new HtmlWebpackPlugin({
       template: './src/index.hbs',
       inject: true,
